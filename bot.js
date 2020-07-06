04da28bf1fe7388d2c892b7d146524e627579341
IceKnack <67892100+IceKnack@users.noreply.github.com>
2020-07-06 13:06:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,6 @@ const TOKEN = process.env.TOKEN;
 const rover = require('rover-api');
 const noblox = require('noblox.js')
 const { default: Axios } = require('axios');
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 bot.login(TOKEN);
 
