299ce06be3c9aa6fe03baee3176f619cf4801249
IceKnack <67892100+IceKnack@users.noreply.github.com>
2020-07-07 06:08:03 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,24 +4,18 @@ const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 const rover = require('rover-api');
 const noblox = require('noblox.js')
-const { default: Axios } = require('axios');
+const {default: Axios} = require('axios');
+let prefix = "!"
 
-bot.on('ready', async() => {
-  console.info(`Logged in as ${bot.user.tag}!`);
-  await noblox.setCookie(process.env.ROBLOXTOKEN)
+bot.on('ready', async () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+    await noblox.setCookie(process.env.ROBLOXTOKEN)
 })
 
 bot.on('message', async (msg) => {
-  if (msg.content == ",status"){
-    msg.reply(`bot is online as: ${bot.user.tag}`)
-  } else if (msg.content.includes(",user")){
-    if (msg.mentions.members.first()){
-      let body = await rover(msg.mentions.members.first().id)
-      msg.reply(`${msg.mentions.members.first().user.username} is ${body.robloxUsername}`)
-    } else {
-      let body = await rover(msg.author.id)
-      msg.reply(`you are ${body.robloxUsername}`)
-    }
+  if (msg.content.startsWith(prefix)){
+    let command = msg.content.slice(2)
+    msg.channel.send(command)
   }
 })
 
